5657f469160bc12e424f6b8c667e03bb6ec037e8
ImRafat <mrafat101@gmail.com>
2023-01-29 18:45:37 -0500

    implementing swiper

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove('menu--open');
 }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
