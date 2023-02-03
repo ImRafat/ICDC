11c3fddd6dcc181d1e492fdd64207597f0238e58
ImRafat <mrafat101@gmail.com>
2023-02-03 11:08:43 -0500

    Finalized the footer changes.

--- js/app.js
+++ js/app.js
@@ -35,7 +35,6 @@ $( document ).ready(function() {
   const hadiths=["Those people who show no mercy will receive no mercy from Allah.","Wisdom and knowledge are things that the believer lacks. He should take them wherever he finds them.","Religion is very easy and whoever overburdens himself in his religion will not be able to continue in that way. So you should not be extremists, but try to be near to perfection and receive the good tidings that you will be rewarded; and gain strength by worshipping in the mornings, the nights.","No father has ever given a child a gift that is superior to good manners.","It is one of the greatest sins that a man should curse his parents."];
   const item = hadiths[Math.floor(Math.random()*(hadiths.length))];
   const hadithElement=document.getElementsByClassName("hadith")[0].textContent=item;
-  
 
 });
 
