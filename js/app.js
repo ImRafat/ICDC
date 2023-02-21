b273ceb4c81eb724379827a7f3c1041c8bec1627
ImRafat <mrafat101@gmail.com>
2023-02-20 19:28:24 -0500

    trying to fix the dissappearing text on click event

--- js/app.js
+++ js/app.js
@@ -32,10 +32,29 @@ $( document ).ready(function() {
     }    
   });
 
-  const hadiths=["Those people who show no mercy will receive no mercy from Allah.","Wisdom and knowledge are things that the believer lacks. He should take them wherever he finds them.","Religion is very easy and whoever overburdens himself in his religion will not be able to continue in that way. So you should not be extremists, but try to be near to perfection and receive the good tidings that you will be rewarded; and gain strength by worshipping in the mornings, the nights.","No father has ever given a child a gift that is superior to good manners.","It is one of the greatest sins that a man should curse his parents."];
-  const item = hadiths[Math.floor(Math.random()*(hadiths.length))];
-  const hadithElement=document.getElementsByClassName("hadith")[0].textContent=item;
+  let hadiths=["Those people who show no mercy will receive no mercy from Allah.","Wisdom and knowledge are things that the believer lacks. He should take them wherever he finds them.","Religion is very easy and whoever overburdens himself in his religion will not be able to continue in that way. So you should not be extremists, but try to be near to perfection and receive the good tidings that you will be rewarded; and gain strength by worshipping in the mornings, the nights.","No father has ever given a child a gift that is superior to good manners.","It is one of the greatest sins that a man should curse his parents."];
+  let item = hadiths[Math.floor(Math.random()*(hadiths.length))];
+  let hadithElement=document.getElementsByClassName("hadith")[0].textContent=item;
+  // document.getElementById("buttonNext").ej2_instances[0].removeEventListener("click");
+
+
+  document.addEventListener('click', function(e) 
+  {     
+
+  
+    // hadithElement=document.getElementsByClassName("hadith")[0].textContent=item;
+  })
+
+  $('body').on('focusin', function(e){
+    // firstSlide.insertAdjacentHTML('afterBegin','this is happening');
+    console.log(e.target.getAttribute('id'));
+    // if(e.target.getAttribute('id')=='buttonNext')
+    // {
+    //   console.log('next button was pressed');
+    // }
+  });
 
 });
+
 
 
